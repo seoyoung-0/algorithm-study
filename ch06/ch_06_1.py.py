646a0118b5a9920a5f1281331d295e2c3d382ea8
Park seoyoung <52600701+seoyoung-0@users.noreply.github.com>
2021-07-16 13:40:52 +0900

    Update ch_06_1.py.py

--- ch06/ch_06_1.py.py
+++ ch06/ch_06_1.py.py
@@ -26,3 +26,14 @@ class Solution:
             return True
         else:
             return False
+        
+        
+        
+        
+# --------------슬라이싱 사용
+import re
+def isPalindrome(self, s: str) -> bool:
+    s = s.lower()
+    s = re.sub('[^a-z0-9]','',s)
+    return s == s[::-1]
+        
