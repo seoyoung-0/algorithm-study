abbc12796ccb45256df49ac25037da6be52adb50
Park seoyoung <52600701+seoyoung-0@users.noreply.github.com>
2021-08-02 13:32:31 +0900

    Update ch_10_2.py

--- ch10/ch_10_2.py
+++ ch10/ch_10_2.py
@@ -1,38 +1,39 @@
-# https://leetcode.com/problems/implement-queue-using-stacks/
-class MyQueue:
+# https://leetcode.com/problems/merge-k-sorted-lists/
+'''
+- k 개의 정렬된 리스트 하나로 합치면서 정렬하기
 
-    def __init__(self):
-        
-        self.input = []
-        self.output = []
-    
-    def push(self, x: int) -> None:
-        
-        self.input.append(x)
+- 리스트 별로 heap 에 저장.
+- 리스트 별 0번째 원소로 정렬(heappush)하면서 heappop()해서 연결
+'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+import heapq
 
-    def pop(self) -> int:
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
         
-        self.peek()
-        return self.output.pop()
+        root = result = ListNode(None)
         
-
-    def peek(self) -> int:
-        print(self.output)
-        if not self.output: ### 
-            while self.input:
-                self.output.append(self.input.pop())
-        return self.output[-1]
+        heap = []
+        ''' 중복되는 루트 값 push 불가능
+        for lst in lists:
+            heapq.heappush(heap, (lst.val, lst))
+        '''  
+        for i in range(len(lists)):
+            if lists[i]:
+                heapq.heappush(heap, (lists[i].val, i,lists[i]))
         
-
-    def empty(self) -> bool:
-         
-        return len(self.input) == 0 and len(self.output)==0
-        
-
-
-# Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.peek()
-# param_4 = obj.empty()
+        #####
+        # next 있으면 다시 heappush ~ push 하면서 재정렬
+        while heap:
+            node = heapq.heappop(heap)
+            idx = node[1]
+            result.next = node[2]
+            result = result.next
+            if result.next:
+                heapq.heappush(heap, (result.next.val, idx, result.next))
+            
+        return root.next
